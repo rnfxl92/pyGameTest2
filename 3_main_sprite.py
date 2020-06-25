89972ccedd453f0f753b2d142fac88bf22aa6e33
sungminpark <rnfxl92@naver.com>
2020-06-26 00:52:15 +0900

    pygame test2

--- 3_main_sprite.py
+++ 3_main_sprite.py
@@ -12,6 +12,13 @@ pygame.display.set_caption("Sungmin Game!") #게임 이름
 # 배경 이미지 불러오기
 background = pygame.image.load("/Users/sungminpark/PycharmProjects/pyGameTest2/background.jpg")
 
+# 캐릭터(스프라이트) 불러오기
+character = pygame.image.load("/Users/sungminpark/PycharmProjects/pyGameTest2/character.jpg")
+character_size = character.get_rect().size # 이미지의 크기를 구해옴
+character_width = character_size[0] # 캐릭터의 가로 크기
+character_height = character_size[1] # 캐릭터의 세로 크기
+character_x_pos = (screen_width // 2)  - (character_width // 2) # 화면 가로의 절반크기에 해당하는 곳에 위치
+character_y_pos = screen_height - character_height # 화면 세로 크기 가장 아래에 위치
 
 
 # 이벤트 루프
@@ -25,8 +32,9 @@ while running:
             running = False
 
 
-    #screen.fill((0, 0, 255))
+
     screen.blit(background, (0, 0)) #배경 그리기
+    screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
 
     pygame.display.update() # 게임화면 다시그리기!
 
